5c30383100601cbfaf8de04e126ecf0b36b599a2
giassuer <g.assuero@hotmail.it>
2021-12-13 12:02:36 +0100

    milestone-3

--- Milestone-3/js/script.js
+++ Milestone-3/js/script.js
@@ -1,7 +1,6 @@
-// Milestone 2
-// Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi
-// relativi al contatto attivo all’interno del pannello della conversazione
-// Click sul contatto mostra la conversazione del contatto cliccato
+// Milestone 3
+// Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+// Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
 
 
 
@@ -115,13 +114,15 @@ const app = new Vue(
 
            newMessage: function(){
             if (this.sendMessage.text !== '') {
-                const messages = 
+                const myMessages = { messages:  [
                 {
                     text: this.sendMessage.text,
                     date: '10/01/2020 15:30:55',
                     status: 'sent'
-                }
-                this.contacts.push(messages)
+                },
+            ],
+             } 
+                this.contacts.push(myMessages)
                 this.sendMessage.text = ''
             }
            }
